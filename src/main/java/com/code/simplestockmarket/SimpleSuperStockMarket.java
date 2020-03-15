bb8ef18f82158dc609371f6f37de4310d0934dd5
krutika-patidar <krutikapatidar@gmail.com>
2020-03-15 23:57:15 +0530

    code formatted

--- src/main/java/com/code/simplestockmarket/SimpleSuperStockMarket.java
+++ src/main/java/com/code/simplestockmarket/SimpleSuperStockMarket.java
@@ -13,7 +13,6 @@ public class SimpleSuperStockMarket {
 
     public static void main(String args[]) {
         System.out.println("Welcome to Simple Super Stock Market!!");
-
         try {
             StockMarketMenu.buildStockMarketExchange();
         } catch (StockMarketException e) {
